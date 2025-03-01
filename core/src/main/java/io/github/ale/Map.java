f529d484e0857e3930099c265dc71c8ea54115c6
alespa727 <alespa20075@gmail.com>
2025-03-01 01:19:44 +0100

    .

--- core/src/main/java/io/github/ale/Map.java
+++ core/src/main/java/io/github/ale/Map.java
@@ -2,22 +2,28 @@ package io.github.ale;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Map {
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
-
+    TiledMapTileLayer layer;
     private static int width;
     private static int height;
 
     public Map(OrthographicCamera camera){
         map = new TmxMapLoader().load("maps/map2.tmx");
+        layer = (TiledMapTileLayer) map.getLayers().get(0);
+
         mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
         width=50;
         height=50;
-        map.getLayers();
+        boolean a = tileSolido(50, 0, layer);
+        System.out.println(a);
         update(camera);
     }
 
@@ -49,4 +55,14 @@ public class Map {
     public static int getHeight(){
         return height;
     }
+    
+    
+    public boolean tileSolido(int x, int y, TiledMapTileLayer layer) {
+        Cell cella = layer.getCell(x, y);
+        if (cella == null) return false;
+
+        TiledMapTile tile = cella.getTile();
+        return tile != null && tile.getProperties().containsKey("collisione");
+    }
+
 }
