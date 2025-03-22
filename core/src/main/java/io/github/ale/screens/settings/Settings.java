8096fc69f47cc4a5bbc1db0b71e59714b6b99425
Tommaso <tomferra07@gmail.com>
2025-03-22 15:30:42 +0100

    aggiunto base settings

--- core/src/main/java/io/github/ale/screens/settings/Settings.java
+++ core/src/main/java/io/github/ale/screens/settings/Settings.java
@@ -3,9 +3,12 @@ package io.github.ale.screens.settings;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import io.github.ale.MyGame;
@@ -21,24 +24,55 @@ public class Settings implements Screen {
         this.game = game;
     }
 
+    @Override
+    public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        root = new Table();
+        root.setFillParent(true);
+        skin = new Skin(Gdx.files.internal("metal-ui.json"));
+        table = new Table();
+
+        /*
+        System.out.println("Java Heap: " + Gdx.app.getJavaHeap() / (1024 * 1024) + " MB");
+        System.out.println("Native Heap: " + Gdx.app.getNativeHeap() / (1024 * 1024) + " MB");
+         */
+
+        // Add some UI elements
+        TextButton backButton = new TextButton("Back", skin);
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(game.mainScreen);
+            }
+        });
+
+        /*
+        System.out.println("Java Heap: " + Gdx.app.getJavaHeap() / (1024 * 1024) + " MB");
+        System.out.println("Native Heap: " + Gdx.app.getNativeHeap() / (1024 * 1024) + " MB");
+         */
+
+        table.add(backButton);
+        root.add(table);
+        stage.addActor(root);
+    }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0.9f, 0.9f, 0.9f, 0.9f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
-        stage.draw();
+        if (stage != null) {
+            stage.act();
+            stage.draw();
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        if (skin != null) skin.dispose();
+        if (stage != null) {
+            stage.getViewport().update(width, height, true);
+        }
     }
 
     @Override
@@ -53,23 +87,19 @@ public class Settings implements Screen {
 
     @Override
     public void hide() {
-        dispose();
+        // Do not dispose here, just clear input processor
+        Gdx.input.setInputProcessor(null);
     }
-
 
     @Override
-    public void show() {
-        stage = new Stage(new ScreenViewport());
-
-        Gdx.input.setInputProcessor(stage);
-        root = new Table();
-        root.setFillParent(true);
-        skin = new Skin(Gdx.files.internal("metal-ui.json"));
-        table = new Table();
-
-        stage.addActor(root);
-        root.add(table);
-
+    public void dispose() {
+        if (stage != null) {
+            stage.dispose();
+            stage = null;
+        }
+        if (skin != null) {
+            skin.dispose();
+            skin = null;
+        }
     }
-
 }
