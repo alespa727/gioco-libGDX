f1988469c56957cae2097126c857e13f8c381109
Tommaso <tomferra07@gmail.com>
2025-04-11 18:19:15 +0200

    .

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -1,56 +1,16 @@
-//texture 0
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+varying vec4 v_color;
+varying vec2 v_texCoords;
+
 uniform sampler2D u_texture;
-
-//our screen resolution, set from Java whenever the display is resized
-uniform vec2 resolution;
-
-//"in" attributes from our vertex shader
-varying vec4 vColor;
-varying vec2 vTexCoord;
-
-//RADIUS of our vignette, where 0.5 results in a circle fitting the screen
-const float RADIUS = 0.8;
-
-//softness of our vignette, between 0.0 and 1.0
-const float SOFTNESS = 0.3;
-
-//sepia colour, adjust to taste
-const vec3 SEPIA = vec3(1.0, 1.0, 1.0);
+uniform float u_time; // Passato dal codice Java
 
 void main() {
-	//sample our texture
-	vec4 texColor = texture2D(u_texture, vTexCoord);
-
-	//1. VIGNETTE
-
-	//determine center position
-	vec2 position = (gl_FragCoord.xy / resolution.xy) - vec2(0.5);
-
-	//determine the vector length of the center position
-	float len = length(position);
-
-	//use smoothstep to create a smooth vignette
-	float vignette = smoothstep(RADIUS, RADIUS-SOFTNESS, len);
-
-	//apply the vignette with 50% opacity
-	texColor.rgb = mix(texColor.rgb, texColor.rgb * vignette, 0.3);
-
-	//2. GRAYSCALE
-
-	//convert to grayscale using NTSC conversion weights
-	float gray = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
-
-	//3. SEPIA
-
-	//create our sepia tone from some constant value
-	vec3 sepiaColor = vec3(gray) * SEPIA;
-
-	//again we'll use mix so that the sepia effect is at 75%
-	texColor.rgb = mix(texColor.rgb, sepiaColor, 0.5);
-
-
-	texColor.rgb = clamp(texColor.rgb * 1.2, 0.0, 1.0);
-
-	//final colour, multiplied by vertex colour
-	gl_FragColor = texColor * vColor;
+    // Scorre la texture verso destra nel tempo
+    vec2 scrolledCoords = vec2(v_texCoords.x + u_time, v_texCoords.y);
+    vec4 texColor = texture2D(u_texture, scrolledCoords);
+    gl_FragColor = texColor * v_color;
 }
--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -1,14 +1,14 @@
-uniform mat4 u_projTrans;
-
 attribute vec4 a_position;
 attribute vec4 a_color;
 attribute vec2 a_texCoord0;
 
-varying vec4 vColor;
-varying vec2 vTexCoord;
+uniform mat4 u_projTrans;
+
+varying vec4 v_color;
+varying vec2 v_texCoords;
 
 void main() {
-    vColor = a_color;
-    vTexCoord = a_texCoord0;
+    v_color = a_color;
+    v_texCoords = a_texCoord0;
     gl_Position = u_projTrans * a_position;
 }
