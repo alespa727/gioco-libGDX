a7d80a95bdee149158a7b53d7c6d279069968f80
alespa727 <alespa20075@gmail.com>
2025-04-15 09:37:47 +0200

    .

--- core/src/main/java/progetto/gameplay/map/MapManager.java
+++ core/src/main/java/progetto/gameplay/map/MapManager.java
@@ -3,9 +3,12 @@ package progetto.gameplay.map;
 import java.util.HashMap;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 import progetto.Core;
@@ -15,6 +18,8 @@ import progetto.gameplay.entity.types.living.combat.boss.Boss;
 import progetto.gameplay.entity.types.living.combat.boss.BossInstance;
 import progetto.gameplay.manager.ManagerEntity;
 import progetto.utils.TerminalCommand;
+
+import javax.crypto.KEMSpi;
 
 public class MapManager {
 
@@ -60,12 +65,19 @@ public class MapManager {
      */
     public void changeMap(int map, float x, float y) {
         if (currentMap != null) { // Se almeno una mappa è stata caricata
+            FileHandle fileHandle = Gdx.files.local("/save/"+ currentMap.nome +".json");
+            Json json = new Json();
             if (mapEntityInstances.get(currentMap.nome)!=null)
                 mapEntityInstances.get(currentMap.nome).clear();
             Array<EntityInstance> instances = managerEntity.clear();
             mapEntityInstances.put(currentMap.nome, instances); // Salva le entità in una hashmap
+            String string = json.toJson(instances);
+            System.out.println(json.prettyPrint(string));
+            fileHandle.writeString(string, false);
             currentMap.dispose(); // Cancellazione mappa precedente
         }
+
+
 
         // Cambio mappa
         switch (map) {
@@ -82,13 +94,13 @@ public class MapManager {
             }
 
             default -> {
-                nome = "map"; // Nome file
+                nome = "stanza"; // Nome file
                 ambienteAperto = true; // Tipo ambiente
                 viewport.setWorldSize(22f, 22f * 9 / 16f); // Grandezza telecamera
             }
         }
 
-        spawnInstances();
+
 
         // Riassegnazione index
         currentMapNum = map;
@@ -96,6 +108,20 @@ public class MapManager {
         // Creazione mappa e crea corpi/eventi rilevanti
         currentMap = new Map(nome, managerEntity, this, x, y);
         currentMap.createCollision();
+
+        if (currentMap!=null){
+            FileHandle file = Gdx.files.local("/save/"+ currentMap.nome +".json");
+            if (file.exists()) {
+                Json json = new Json();
+                Array<EntityInstance> loadedInstances = json.fromJson(Array.class, EntityInstance.class, file);
+                System.out.println("TESTO DA FILE" + loadedInstances);
+                for (int i = 0; i < loadedInstances.size; i++) {
+                    EntityInstance instance = loadedInstances.get(i);
+                    instance.loadTexture();
+                }
+                managerEntity.summon(loadedInstances);
+            }
+        }
 
 
         // Applico la telecamera
