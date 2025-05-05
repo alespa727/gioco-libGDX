16c58bd08115463c74dbfd666cf2e41ad97ad359
alespa727 <alespa20075@gmail.com>
2025-05-05 19:38:04 +0200

    .

--- core/src/main/java/progetto/core/game/TextAnimator.java
+++ core/src/main/java/progetto/core/game/TextAnimator.java
@@ -8,9 +8,8 @@ import com.badlogic.gdx.math.MathUtils;
 public class TextAnimator {
     private boolean hasFinished = true;
     private String text;
-    private StringBuilder currentText;
+    private final StringBuilder currentText;
     private float timeAccumulator;
-    private float baseTimePerLetter;
     private float timePerLetter;
 
     public TextAnimator() {
@@ -26,17 +25,8 @@ public class TextAnimator {
         this.text = text;
         this.currentText.setLength(0);
         this.hasFinished = false;
-        int length = text.length();
-        float minSpeed = 0.001f;
-        float maxSpeed = 0.4f;
 
-        baseTimePerLetter = MathUtils.clamp(
-                maxSpeed - (length / 40f) * (maxSpeed - minSpeed),
-                minSpeed,
-                maxSpeed
-        );
-
-        randomizeTimePerLetter();
+        this.timePerLetter = 0.045f;
     }
 
     public void update(float delta) {
@@ -46,14 +36,9 @@ public class TextAnimator {
             int nextIndex = currentText.length();
             if (nextIndex < text.length()) {
                 currentText.append(text.charAt(nextIndex));
-                randomizeTimePerLetter();
             }else hasFinished = true;
             timeAccumulator -= timePerLetter;
         }
-    }
-
-    private void randomizeTimePerLetter() {
-        this.timePerLetter = Math.min(baseTimePerLetter, 0.4f);
     }
 
     public String getCurrentText() {
